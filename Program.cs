9e2f0997cb64858c669af30c18aee2cb0eaf3d13
Kaizen119 <sean.b.tomberlin1@gmail.com>
2023-01-18 01:00:14 -0500

    update

--- Program.cs
+++ Program.cs
@@ -6,12 +6,15 @@ global using server.Dtos.WorkOrder;
 global using server.Dtos.Technician;
 global using Microsoft.EntityFrameworkCore;
 global using server.Data;
-using Microsoft.AspNetCore.Connections;
+global using Microsoft.AspNetCore.Cors.Infrastructure;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Connections;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddDbContext<DataContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,6 +23,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<IWorkOrderService, WorkOrderService>();
 builder.Services.AddScoped<ITechnicianService, TechnicianService>();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyHeader();
+        }
+    );
+});
+
 
 var app = builder.Build();
 
@@ -28,7 +41,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors();
 }
+
+app.UseRouting();
 
 app.UseHttpsRedirection();
 
